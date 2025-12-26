e27314180e63d9d0936922ef99987ea9a9a0491f
juan ormeno <juan.pablo.ormeno.farias@gmail.com>
2025-12-26 20:38:39 -0300

    inicio proyecto

---
+++ src/test/java/org/bank/issuingbank/IssuingBankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bank.issuingbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuingBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
